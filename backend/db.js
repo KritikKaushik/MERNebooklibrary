103f7d09ca272924fa20a4d21951bda92d3c8896
Kritik Kaushik <kritikkaushik2004@gmail.com>
2026-07-29 21:30:27 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,6 @@ const connectDB = async () => {
     const connection = await mongoose.connect(
       process.env.MONGO_URI
     );
-
     console.log(
       `MongoDB Connected: ${connection.connection.host}`
     );
